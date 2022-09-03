8e0eb56315b846459cf4afa18b778057014ddf41
joko-santoso <hello.jokosantoso@gmail.com>
2022-09-03 21:48:13 +0700

    Update : api gateway basic auth feature

---
+++ packages/api-gateway/src/auth/constants.ts
@@ -0,0 +1,14 @@
+export const jwtConstants = {
+  secret: 'VsXu2x4z6CTheOrphanbGeKgNjRnTqWt',
+  expired: '12h',
+};
+// y6B8DbGdJfNTheOrphanmSqVsXu2x4z7
+// FcHfMhPkSTheOrphanrWuZw3z6B8DbGd
+// NkRnTqWtYv3y5ATheOrphanaFcHfMhPk
+// VsXTheOrphanx4z7C9EbHeKgNkRnTrWt
+// 6B8DbGdJfNjQmSqVsXv2TheOrphanz7C
+// HfMhPkSpUrTheOrphanZw3z6B8DbGdJf
+// TrWtYTheOrphany5A7DaFcHfMhPmSpUr
+// mTqVsXv2x4z7C9EbHeKTheOrphankRnT
+// PmSpUrXuZw3z6TheOrphanDbGdJgNjQm
+// NkRnTrWtTheOrphan3y5A7DaFcJfMhPm
